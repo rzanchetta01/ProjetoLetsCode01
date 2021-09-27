b4e40d5e9c6f7ad397c3135cc0633a5ee236220a
rzanchetta01 <rzanchetta01@gmail.com>
2021-09-27 20:08:08 -0300

    test

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoLesCode01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
