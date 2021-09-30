e9f1745bbff3b81ef9d2f5a4c23aada2663cfeb6
Samantha1024a-l <samanthaalvarengaa.l23@gmail.com>
2021-09-30 20:35:21 -0300

    Update SelfService.cs

--- SelfService.cs
+++ SelfService.cs
@@ -2,39 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-/*
-IMPLEMENTAÇÃO BASE DA CLASE
-
-Precisa adcionar os metodos de preparar pedidos e adcionar os pratos no cardapio
-
-*/
 
 namespace ProjetoLesCode01
 {
     public class SelfService: ILojas
     {
-        private string nomeSelfService {get; set;}
-        private string posShop {get;}
-
+        private string nomeSelfService;
+        private string posShop;
         private List<String> cardapio = new List<String>();
         
-
-        public string NomeLoja {
-
-            get  { return this.nomeSelfService; }           
+        public string NomeLoja 
+        {
+            get  { return this.nomeSelfService; }   
+            set {nomeSelfService = value; }        
         }
-        public string PosShop {
-
-            get  {return this.posShop; }
+        public string PosShop
+        {
+          get  {return posShop; }
         }
-
-        public List<String> Cardapio {
-
-            get { return this.cardapio; }          
-        }
-        public List<String> Pedidos {
-
-            get { return this.pedidos; }           
+        public List<String> Cardapio
+        {
+          get { return cardapio; }          
         }
 
         public SelfService(String nomeSelfService, string posShop)
@@ -43,5 +31,36 @@ namespace ProjetoLesCode01
             this.nomeSelfService = nomeSelfService;
         }
 
-    }
+        public void AddCardapio(String algo)
+        {
+            cardapio.Add(algo);
+        }
+
+        public void MostrarCardapio()
+        {
+            foreach(var e in cardapio)
+            {
+                Console.WriteLine(e);
+            }            
+        }
+
+        public void RemoverDoCardapio(String algo)
+        {
+           Cardapio.Remove(algo);
+        }
+
+        public double RealizarVenda(List<String> itens)
+        {
+            double precoTotal = 0;
+
+            foreach(var e in itens)
+            {
+                precoTotal = precoTotal + 3;
+            }
+
+            return precoTotal;
+        }
+
+
+    }  
 }
